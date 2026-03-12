b5a817020594c0288901f03935cb86932aa3bfb2
Lain-Ego <2302109068@qq.com>
2026-03-12 10:54:26 +0800

    feat(env): add HTDW4438 V2 task config and registry
    - add standalone htdw_4438_v2 env config module
    
    - register new task name htdw_4438_v2 in task registry
    
    - keep existing htdw_4438 task unchanged

--- legged_gym/envs/__init__.py
+++ legged_gym/envs/__init__.py
@@ -2,9 +2,11 @@ from .base.legged_robot import LeggedRobot
 from .a1.a1_config import A1RoughCfg, A1RoughCfgPPO
 from .go1.go1_config import Go1RoughCfg, Go1RoughCfgPPO
 from .htdw_4438.htdw_4438_config import Htdw4438Cfg, Htdw4438CfgPPO
+from .htdw_4438_v2.htdw_4438_v2_config import Htdw4438V2Cfg, Htdw4438V2CfgPPO
 
 from legged_gym.utils.task_registry import task_registry
 
 task_registry.register("a1", LeggedRobot, A1RoughCfg(), A1RoughCfgPPO())
 task_registry.register("go1", LeggedRobot, Go1RoughCfg(), Go1RoughCfgPPO())
 task_registry.register("htdw_4438", LeggedRobot, Htdw4438Cfg(), Htdw4438CfgPPO())
+task_registry.register("htdw_4438_v2", LeggedRobot, Htdw4438V2Cfg(), Htdw4438V2CfgPPO())
